44cb941494aa90e9ecdbce0a2a533e4295ab61ee
Pigletto <pigletto@gmail.com>
2013-03-12 19:10:20 +0100

    fix

--- lfs_sofortueberweisung/urls.py
+++ lfs_sofortueberweisung/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('lfs_sofortueberweisung.views',
-                       url(r'^notification_url/(?P<order_id>\d*)/(?P<security_hash>).*?/$',
+                       url(r'^notification_url/(?P<order_id>\d*)/(?P<security_hash>.*?)/$',
                            "success_notification", name="lfs_sofortueberweisung_notification"),
 )
